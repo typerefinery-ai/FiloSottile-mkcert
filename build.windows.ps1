4b157f9946addd18866f24696c3744cae9953a41
wildone <max.barrass@gmail.com>
2023-06-27 16:23:00 +1000

    add win32 build.

--- build.windows.ps1
+++ build.windows.ps1
@@ -1,9 +1,9 @@
 # env GOOS=windows GOARCH=amd64 
-go build -o ${PWD}\bin\win32\localcert.exe .\cmd\localcert\main.go
+go build -o ${PWD}/bin/win32/mkcert.exe -ldflags "-X main.Version=1.4.4"
 if(!$?) {
     Write-Host "Build failed."
     exit 1
 }
-dir ${PWD}\bin\
-mkdir data -ErrorAction SilentlyContinue
-.\bin\win32\localcert.exe -dataDir ${PWD}\data -acceptTerms -localCert ${PWD}\\data\\cert.pem -localKey ${PWD}\\data\\key.pem -forceRenew -debug
+# dir ${PWD}\bin\
+# mkdir data -ErrorAction SilentlyContinue
+# .\bin\win32\localcert.exe -dataDir ${PWD}\data -acceptTerms -localCert ${PWD}\\data\\cert.pem -localKey ${PWD}\\data\\key.pem -forceRenew -debug
